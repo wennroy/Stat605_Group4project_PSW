008744b3ba92fe68de68d0c6041694a2a19456a9
wennroy <wennroy@gmail.com>
2021-12-15 11:07:37 -0600

    Delete the orginal codes

--- Codes/CHTC_Codes/mv_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mv mspectra* spec/
-mv label_file* label/
-
-tar -cvf spec.tar spec/
-tar -cvf label.tar label/
--- Codes/CHTC_Codes/submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir output
-mkdir err
-mkdir log
-echo "Downloading the data might take 5 hours and longer since the data are stored in my personal server. It has internet speed restriction."
-condor_submit_dag birdsong_analysis.dag
